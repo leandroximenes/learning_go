873b8662091f626c6f328982e90ec1a75d3ebe60
Leandro <leandroj.r.ximenes@gmail.com>
2024-09-24 14:53:52 +0000

    first endpoint created

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"item"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clear Room", Completed: true},
+	{ID: "2", Item: "Read Book", Completed: false},
+	{ID: "3", Item: "Interesting Video", Completed: true},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+
+}
 
 func main() {
-	fmt.Println("hello world")
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:9090")
 }
