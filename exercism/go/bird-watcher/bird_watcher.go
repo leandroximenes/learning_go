8efa52bb2bcd08a912100a4b678be1108b0ae465
Leandro <leandroj.r.ximenes@gmail.com>
2024-11-02 02:03:35 +0000

    feat(exercism): for loops

---
+++ exercism/go/bird-watcher/bird_watcher.go
@@ -0,0 +1,36 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	total := 0
+	for i := 0; i < len(birdsPerDay); i++ {
+		total += birdsPerDay[i]
+	}
+	return total
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+    week --
+	weekStarted := week * 7
+	weekEnd := weekStarted + 7
+	total := 0
+	
+	for i := weekStarted; i < weekEnd; i++ {
+		total += birdsPerDay[i]
+	}
+	return total
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i++ {
+		if i%2 == 0 {
+			birdsPerDay[i] ++
+		} 
+	}
+	return birdsPerDay
+}
