ed7d1a05cf20f348a9fe5f1916fce4ff3f5f4755
Leandro <leandroj.r.ximenes@gmail.com>
2026-03-30 21:33:22 -0300

    chore(day9): finished the exercice.

--- practicing/day 9/main.go
+++ practicing/day 9/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Status string
 
@@ -21,21 +24,29 @@ func main() {
 
 	users := map[string]*User{
 		"a1": newUser("a1", "Leandro", "Ximenes"),
-		"c3": {ID: "c3", Name: "Sara", LastName: "Serpa", Status: InActive},
+		"b2": newUser("b2", "Alessandra", "Serpa"),
+		"c3": &User{ID: "c3", Name: "Sara", LastName: "Serpa", Status: InActive},
 	}
 
-	users["a2"] = newUser("b2", "Alessandra", "Serpa")
-
 	ActivateUsers(users, "a1", "b2", "c3")
+	userList := ListActiveUsers(users)
 
 	for _, user := range users {
 		fmt.Printf("%+v\n", *user)
 	}
 
+	sort.Slice(userList, func(i, j int) bool {
+		return userList[i].Name < userList[j].Name
+	})
 }
 
 func newUser(id, name, lastName string) (u *User) {
-	user := User{ID: id, Name: name, LastName: lastName, Status: Active}
+	user := User{
+		ID:       id,
+		Name:     name,
+		LastName: lastName,
+		Status:   Active,
+	}
 
 	return &user
 }
@@ -43,9 +54,20 @@ func newUser(id, name, lastName string) (u *User) {
 func ActivateUsers(users map[string]*User, ids ...string) {
 	for _, id := range ids {
 		user, isUser := users[id]
-		
+
 		if isUser {
 			user.Status = Active
 		}
 	}
+}
+
+func ListActiveUsers(users map[string]*User) []User {
+	var activeUsers []User
+	for _, user := range users {
+		if user.Status == Active {
+			activeUsers = append(activeUsers, *user)
+		}
+	}
+
+	return activeUsers
 }
