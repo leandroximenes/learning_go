176dcb9ac361a9b63620f4fc2e78c79324fc37ee
Leandro <leandroj.r.ximenes@gmail.com>
2026-04-07 22:26:58 -0300

    chore(day13): fixing knolege about defer

--- practicing/day 13/main.go
+++ practicing/day 13/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Notifier interface {
 	Notify() string
@@ -13,6 +15,9 @@ type Email struct {
 }
 
 func (e Email) Notify() string {
+	if e.To == "" {
+		panic("To is required")
+	}
 	return fmt.Sprintf("Sending email to %s", e.To)
 }
 
@@ -30,12 +35,30 @@ func SendMessage(e Notifier) string {
 	return e.Notify()
 }
 
+func SendSave(e Notifier) string {
+	defer func() {
+		if r := recover(); r != "" {
+			fmt.Println("Erro ao enviar notificação")
+		}
+	}()
+
+	return SendMessage(e)
+}
+
+func testDeferComRetorno() string {
+	defer fmt.Println("Teste na função")
+	return "hello World"
+}
+
 func main() {
-	email := Email{To: "Leandro", From: "Alessandra", Message: "Enviando um email "}
+	defer fmt.Println("Finalizado o programa com defer")
+	saudacao := testDeferComRetorno()
+	fmt.Print(saudacao)
+	email := Email{From: "Alessandra", Message: "Enviando um email "}
 	sms := SMS{To: "Sara", From: "Sofia", Message: "Enviando um SMS "}
 
-	emailMessage := SendMessage(email)
-	smsMessage := SendMessage(sms)
+	emailMessage := SendSave(email)
+	smsMessage := SendSave(sms)
 
 	fmt.Println(emailMessage)
 	fmt.Println(smsMessage)
